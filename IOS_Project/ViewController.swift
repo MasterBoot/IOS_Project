e6e1bb2e84f97997f7bf60ad7bf6bd6063cf8063
MasterBoot <1055258439@qq.com>
2016-03-17 11:22:01 +0800

    iOS应用程序x+y
    iOS应用程序x+y

--- IOS_Project/ViewController.swift
+++ IOS_Project/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var c: UITextField!
+    @IBOutlet weak var b: UITextField!
+    @IBOutlet weak var a: UITextField!
+    @IBAction func add(sender: UIButton) {
+        //print("hello world");
+        //c = a+b;
+        var ta:Double!=0;
+        var tb:Double!=0;
+        var tc:Double!=0;
+        
+        if(!a.text!.isEmpty)//判断是否为空
+        {
+            ta=(a.text! as NSString).doubleValue
+        }
+        if(!b.text!.isEmpty)
+        {
+            //tb=(b.text! as NSString).doubleValue
+            tb=(Double)(b.text!);
+        }
+        tc=ta+tb;
+        c.text="\(tc)";
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +45,3 @@ class ViewController: UIViewController {
 
 
 }
-
