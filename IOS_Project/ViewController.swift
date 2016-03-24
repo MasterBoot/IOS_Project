c09d50fc6de9a37072f2fc4d0071d659e49de2ca
MasterBoot <1055258439@qq.com>
2016-03-24 11:42:12 +0800

    ios计算器_第六节课
    修复bug，解决小数点重复问题，解决0开头数字问题，解决单机等于号程序异常退出问题，新增求方功能，新增求摸功能

--- IOS_Project/ViewController.swift
+++ IOS_Project/ViewController.swift
@@ -10,16 +10,144 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var c: UITextField!
-    @IBOutlet weak var b: UITextField!
-    @IBOutlet weak var a: UITextField!
-    @IBAction func add(sender: UIButton) {
+    var loop=0;
+    var x=0.0;
+    var y=0.0;
+    var pointLoop=false;
+    @IBAction func x_y(sender: UIButton) {
+        xSet();
+        c.text!="\(x*x*x)";
+    }
+    @IBAction func x_2(sender: UIButton) {
+        xSet();
+        c.text!="\(x*x)";
+    }
+    @IBAction func x_mode(sender: UIButton) {
+        xSet();
+        c.text!="";
+        pointLoop=false;
+        loop=5;
+    }
+    @IBAction func Add_1(sender: AnyObject) {
+        zeroSet();
+        c.text! += "1";
+    }
+    @IBAction func Add_2(sender: UIButton) {
+        zeroSet();
+        c.text! += "2";
+    }
+    @IBAction func Add_3(sender: UIButton) {
+        zeroSet();
+        c.text! += "3";
+    }
+    @IBAction func Add_4(sender: UIButton) {
+        zeroSet()
+        c.text! += "4";
+    }
+    @IBAction func Add_5(sender: UIButton) {
+        zeroSet();
+        c.text! += "5";
+    }
+    @IBAction func Add_6(sender: UIButton) {
+        zeroSet();
+        c.text! += "6";
+    }
+    @IBAction func Add_7(sender: UIButton) {
+        zeroSet();
+        c.text! += "7";
+    }
+    @IBAction func Add_8(sender: UIButton) {
+        zeroSet();
+        c.text! += "8";
+    }
+    @IBAction func Add_9(sender: UIButton) {
+        zeroSet();
+        c.text! += "9";
+    }
+    @IBAction func Add_0(sender: UIButton) {
+        if(c.text!.isEmpty)
+        {
+            c.text! = "0";
+        }
+        else
+        {
+            zeroSet();
+            c.text! += "0";
+        }
+        
+    }
+    @IBAction func Add_Point(sender: UIButton) {
+        if(!pointLoop)
+        {
+            if(!(c.text!==""))
+            {
+                c.text! += ".";
+                pointLoop=true;
+            }
+            
+        }
+        
+    }
+    @IBAction func Add_JIA(sender: UIButton) {
+        xSet();
+        c.text! = "";
+        pointLoop=false;
+        loop=1;
+    }
+    @IBAction func Add_JIAN(sender: UIButton) {
+        xSet();
+        //c.text! += "-";
+        c.text!="";
+        pointLoop=false;
+        loop=2;
+    }
+    @IBAction func Add_CHENG(sender: UIButton) {
+        xSet();
+        //c.text! += "*";
+        c.text!="";
+        pointLoop=false;
+        loop=3;
+    }
+    @IBAction func Add_CHU(sender: UIButton) {
+        xSet();
+        //c.text! += "/";
+        c.text!="";
+        pointLoop=false;
+        loop=4;
+    }
+    @IBAction func Count(sender: UIButton) {
+        ySet();
+        switch (loop)
+        {
+        case 0:
+            c.text!="0";break;
+        case 1:
+            c.text!="\(x+y)";break;
+        case 2:
+            c.text!="\(x-y)";break;
+        case 3:
+            c.text!="\(x*y)";break;
+        case 4:
+            c.text!="\(x/y)";break;
+        case 5:
+            c.text!="\(x%y)";break;
+        default:
+            c.text!="0";
+        }
+    }
+    @IBAction func Clear(sender: UIButton) {
+        c.text!="";
+        x=0.0;
+        y=0.0;
+        pointLoop=false;
+    }
         //print("hello world");
         //c = a+b;
-        var ta:Double!=0;
-        var tb:Double!=0;
-        var tc:Double!=0;
+        //var ta:Double!=0;
+        //var tb:Double!=0;
+        //var tc:Double!=0;
         
-        if(!a.text!.isEmpty)//判断是否为空
+        /*if(!a.text!.isEmpty)//判断是否为空
         {
             ta=(a.text! as NSString).doubleValue
         }
@@ -29,10 +157,38 @@ class ViewController: UIViewController {
             tb=(Double)(b.text!);
         }
         tc=ta+tb;
-        c.text="\(tc)";
-        
+        c.text="\(tc)";*/
+    func xSet(){
+        if(!c.text!.isEmpty)
+        {
+            x=(Double)(c.text!)!;
+        }
+        else
+        {
+            c.text!="0";
+        }
     }
-
+    func ySet(){
+        if(!c.text!.isEmpty)
+        {
+            y=(Double)(c.text!)!;
+        }
+        else
+        {
+            c.text!="0";
+        }
+    }
+    func zeroSet()
+    {
+        if(c.text!=="0")
+        {
+            c.text!="";
+        }
+    }
+    func cutSet()
+    {
+        //var str = c.text;
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
